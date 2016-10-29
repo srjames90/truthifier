6e98c868ae1905597002166f614d058edf614334
Steven <srjames90@berkeley.edu>
2016-10-29 02:53:30 -0700

    Generating table with proper output. TODO: maybe some styling and a legend for directions

--- public/javascripts/truthifier.js
+++ public/javascripts/truthifier.js
@@ -1,7 +1,7 @@
 (function() {
 	$(document).ready(function() {
 		$('#truthify-button').on('click', function() {
-			toPostfix($('#logic-input').val());
+			generateTable($('#logic-input').val());
 			/*$.post(location, {
 				logicStatement: $(this).val()
 			})
@@ -15,7 +15,7 @@
 		});
 	});
 
-	function toPostfix(logicString) {
+	function generateTable(logicString) {
 		//Don't Accept epty strings
 		if(!logicString) {
 			alert('No logic provided');
@@ -23,7 +23,6 @@
 		}
 		//Stack to help conversion form infix to postfix
 		var stack = [];
-		
 		//Sets the order of operations
 		const order = {
 			'~': 5,
@@ -33,7 +32,6 @@
 			'=': 1,
 			')': 0
 		}
-
 		//regex for parsing the string provided
 		const regex = /\(|p\d+|\)|~|\^|v|>|=/g;
 		var match;
@@ -71,12 +69,50 @@
 		for(var i = 0; i < stack.length;) {
 		    postfix.push(stack.pop());
 		}
-		evaluatePostfix(postfix, props);
+		//Get our table data
+		var tableData = evaluatePostfix(postfix, props);
+		if(!tableData) {
+			alert('Misformed logical statement');
+			return;
+		}
+		console.log(tableData);
+		//sort the keys so that table will look more natural
+		var sortedRowKeys = Object.keys(tableData).sort(function strDes(a, b) {
+			if (a>b) return -1;
+			else if (a<b) return 1;
+			else return 0;
+		});
+		//sort the keys of our propositions
+		var sortedPropKeys = Object.keys(props).sort();
+		//Get the table element
+		$table = $('#truth-table');
+		//empty any existing data
+		$table.empty();
+		//Create header using logical props and our expression
+		$tableHeader = $('<thead></thead>');
+		$tableHeaderRow = $('<tr></tr>');
+		sortedPropKeys.forEach(function(prop) {
+			$tableHeaderRow.append('<th class="text-center">' + prop + '</th>');
+		});
+		$tableHeaderRow.append('<th class="text-center">' + logicString + '</th>');
+		$tableHeader.append($tableHeaderRow);
+		$table.append($tableHeader);
+		//Create rows with truth table data
+		sortedRowKeys.forEach(function(leadRowData) {
+			var row = $('<tr></tr>');
+			var leadRowDataStr = String(leadRowData);
+			for(var i = 0; i < leadRowDataStr.length; i++) {
+				row.append('<td class="text-center">' + (leadRowDataStr.charAt(i) === '1' ?'T':'F') + '</td>');
+			}
+			row.append('<td class="text-center">' + (tableData[leadRowData]?'T':'F') + '</td>');
+			$table.append(row); 
+		});
+
 	}
 
 	function evaluatePostfix(postfix, props) {
 		var length = Object.keys(props).length;
-
+		var tableData = {};
 		//Generate each left hand row based on length of propositions
 		for(var i = 0; i < 2**length; i++) {
 			var row = pad((i).toString(2),length);
@@ -89,29 +125,28 @@
 			}
 			//Go through postfix array and do operations
 			for(oper of postfix) {
-				try {
-					//Check if it's an operand
-					if ((/p\d/).test(oper)) {
-						stack.push(props[oper]);
-					} else if (oper === '~') {
-						console.log(stack);
-						stack.push(eval(stack.pop(), null, oper));
-					} else {
-						var second = stack.pop();
-						var first = stack.pop();
-						//evaluate expressions
-						stack.push(eval(first, second, oper));
-					}
-				} catch(e) {
-					alert('Misformed logical statement');
-					return;
+				//Check if it's an operand
+				if ((/p\d/).test(oper)) {
+					stack.push(props[oper]);
+				} else if (oper === '~') {
+					console.log(stack);
+					stack.push(eval(stack.pop(), null, oper));
+				} else {
+					var second = stack.pop();
+					var first = stack.pop();
+					//evaluate expressions
+					stack.push(eval(first, second, oper));
 				}
 			}
+			var result = stack.pop()
+			if(result !== 0 && result !== 1)
+				return false;
+			tableData[row] = result;
 		}
+		return tableData;
 	}
 
 	function eval(p, q, operator) {
-
 		switch(operator) {
 			case '~':
 				return Number(!p);
@@ -122,7 +157,7 @@
 			case '>':
 				return Number(!(p===1 && q===0))
 			case '=':
-				return p === q;
+				return Number(p === q);
 		}	
 	}
 
