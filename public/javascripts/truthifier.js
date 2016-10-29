2414a14fc3a868dcc016b68e295619a521a0f686
Steven <srjames90@berkeley.edu>
2016-10-29 00:17:38 -0700

    Added infix to postfix method, add code to generate left-hand side of the truth table, and beginning work on evaluating logical proposition based on left-hand side

--- public/javascripts/truthifier.js
+++ public/javascripts/truthifier.js
@@ -37,9 +37,10 @@
 		//regex for parsing the string provided
 		const regex = /\(|p\d|\)|~|\^|v|>|=/g;
 		var match;
-
-		var postfix = '';
-
+		//Initialize postfix string
+		var postfix = [];
+		//initialize object to store truth values of propositions
+		var props = {};
 		//Access the match and turn it from infix to postfix
 		while ((m = regex.exec(logicString)) !== null) {
 		    // This is necessary to avoid infinite loops with zero-width matches
@@ -54,21 +55,60 @@
 		        else if(token === ')') {
 		        	var topStack = stack.pop();
 		        	while(topStack !== '(') {
-		        		postfix += topStack + ' ';
+		        		postfix.push(topStack);
 		        		topStack = stack.pop();
 		        	}
 		        } else if (token in order) {
 		        	while(stack.length && order[stack[stack.length-1]] >= order[token])
-		        		postfix += stack.pop() + ' ';
+		        		postfix.push(stack.pop());
 		        	stack.push(token);
 		        } else {
-		        	postfix += token + ' ';
+		        	props[token] = 0;
+		        	postfix.push(token);
 		        }
 		    });
 		}
 		for(var i = 0; i < stack.length;) {
-		    postfix += stack.pop() + ' ';
+		    postfix.push(stack.pop());
 		}
-		console.log(postfix);
+		evaluatePostfix(postfix, props);
+	}
+
+	function evaluatePostfix(postfix, props) {
+		var length = Object.keys(props).length;
+
+		//Generate each left hand row based on length of propositions
+		//Change prop values to represent these new values
+		for(var i = 0; i < 2**length; i++) {
+			var row = pad((i).toString(2),length);
+			var j = 0
+			for(prop in props) {
+				props[prop] = row[j];
+				j++;
+			}
+			console.log(props);
+			
+		}
+	}
+
+	function eval(p, q, operator) {
+		switch(operator) {
+			case '~':
+				break;
+			case '^':
+				break;
+			case 'v':
+				break;
+			case '>':
+				break;
+			case '=':
+				break;
+		}	
+	}
+
+	function pad(num, size) {
+	    var s = num+"";
+	    while (s.length < size) s = "0" + s;
+	    return s;
 	}
 })();
